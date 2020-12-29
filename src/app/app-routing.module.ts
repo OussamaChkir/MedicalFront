85bef75dd3a27cc38e50768020996f95b62b7601
oussama <oussama501chkir@gmail.com>
2020-12-29 22:58:53 +0100

    add header menu dashboard login register and notfound component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './common/login/login.component';
+import { RegisterComponent } from './common/register/register.component';
+import { DashboardComponent } from './common/dashboard/dashboard.component';
+import { NotfoundComponent } from './common/notfound/notfound.component';
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', component: DashboardComponent },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './common/main-menu/main-menu.component';
+import { HeaderComponent } from './common/main-menu/header/header.component';
+import { LoginComponent } from './common/login/login.component';
+import { RegisterComponent } from './common/register/register.component';
+import { DashboardComponent } from './common/dashboard/dashboard.component';
+import { NotfoundComponent } from './common/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
